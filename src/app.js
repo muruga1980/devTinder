9b30c3662c7c1283c2a786cf8872913050f90030
Murugan <murugaskpm@gmail.com>
2024-09-26 11:47:41 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -4,12 +4,64 @@ const app = express();
 
 // multiple request handler way
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Murugan", age: 40, role: "UI/UX" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("save the  datas from DB");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Delete the datas from DB");
+});
+
 app.use("/home", (req, res) => {
   res.send("this is homepageAAABBBBBBBBBBB");
 });
 
-app.use("/about", (req, res) => {
-  res.send("this is about us page");
+// expression used in routing
+
+//option
+app.post("/abo?ut", (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post("/abo+ut", (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post("/abo*ut", (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post("/a(bo)?ut", (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post("/a(bo)+ut", (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post("/a(bo)*ut", (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post(/a/, (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post(/.*fly$/, (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post("/user/:userId", (req, res) => {
+  res.send("expression keyword used in routin");
+});
+
+app.post("/user/:userId/:name/:pasword", (req, res) => {
+  console.log(req.query);
+  res.send({ name: "murugan", age: 40, password: "hellow123" });
 });
 
 app.use((req, res) => {
